c9ea02b73dd1c0dbab9776666ff7ee0eb475c9e2
Lorsuz <ariel2005souza@gmail.com>
2024-12-15 00:57:47 -0300

    update fetchApi.ts

--- client/src/utils/services/fetchApi.ts
+++ client/src/utils/services/fetchApi.ts
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-const fetchApi = async (
-  route: string,
-  data?: {},
-  method?: "GET" | "POST" | "PUT" | "DELETE"
-) => {
-  try {
-    const response = await fetch(`http://localhost:3001/api${  /* process.env.API_BASE_URL || */ route }`, {
-      method: method || "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: data ? JSON.stringify(data) : undefined,
-    });
 
-    if (!response.ok) {
-      const errorData = await response.json();
-      console.error("Erro ao criar usuário" + errorData);
-      throw new Error(errorData.message || "Erro na requisição");
-    }
-    return await response.json();
-  } catch (error) {
-    console.error("Erro ao criar usuário" + error);
-    throw new Error(error instanceof Error ? error.message : "Erro inesperado");
-  }
-=======
 import { toast } from "react-toastify";
 
 const fetchApi = async ( route: string, data?: {}, method?: "GET" | "POST" | "PUT" | "DELETE" ) => {
@@ -51,7 +25,6 @@ const fetchApi = async ( route: string, data?: {}, method?: "GET" | "POST" | "PU
 		toast.error(error.message)
 		return undefined;
 	}
->>>>>>> 3682d80ef11ca5e855a54cb67eb419ba959e1d68
 };
 
 export default fetchApi;
